23f6bf3d4d3e98b19a3e9379e53fc3f4db0e9b85
Anupam-dagar <dagaranupam@gmail.com>
2017-08-15 23:04:18 +0530

    changes

--- assets/js/abc.js
+++ assets/js/abc.js
@@ -1,15 +1,44 @@
+// variables here
 var animal = document.getElementById("add");
 var btn = document.getElementById("userInput");
+var avatar = document.getElementById("avatar");
+var userName = document.getElementById("username");
+
+//function to be called on submit
 btn.addEventListener("click", function(){
 	var ourRequest = new XMLHttpRequest();
+	var ourRequest2 = new XMLHttpRequest();
 	ourRequest.open('GET', 'https://api.github.com/users/Anupam-dagar/starred');
+	ourRequest2.open('GET', 'https://api.github.com/users/Anupam-dagar');
 	ourRequest.onload = function(){
 		var ourData = JSON.parse(ourRequest.responseText);
 		renderHTML(ourData);
 		console.log(ourData);
 	};
+	ourRequest2.onload = function(){
+		var newData = JSON.parse(ourRequest2.responseText);
+		rendernow(newData);
+		console.log(newData);
+	};
 	ourRequest.send();
+	ourRequest2.send();
 });
+
+// various functions for api call
+function rendernow(somedata) {
+	var somestring = "";
+
+	avatar.src = somedata.avatar_url;
+
+	document.getElementById("githuburl").href = somedata.html_url;
+
+	userName.insertAdjacentHTML('afterbegin', somedata.name);
+
+	document.getElementById("bio").insertAdjacentHTML('afterbegin', somedata.bio);
+
+	somestring += "followers:  " + somedata.followers + "<br>" + "following:  " + somedata.following;
+	document.getElementById("follow").insertAdjacentHTML('afterbegin', somestring);
+}
 function renderHTML(data) {
 	var htmlString = "";
 	var j = 1;
